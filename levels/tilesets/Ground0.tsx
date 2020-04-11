1c19cafb33e87a6b6400691657e814b189c9a692
WyattBaldree <wyattmbaldree@gmail.com>
2020-04-11 18:04:54 -0400

    Level 1 and 2 have been updated.

---
+++ levels/tilesets/Ground0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Ground0" tilewidth="16" tileheight="16" tilecount="56" columns="8">
+ <image source="../img/Ground0.png" width="128" height="112"/>
+</tileset>
---
+++ levels/tilesets/Pit0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Pit0" tilewidth="16" tileheight="16" tilecount="256" columns="8">
+ <image source="../img/Pit0.png" width="128" height="512"/>
+</tileset>
